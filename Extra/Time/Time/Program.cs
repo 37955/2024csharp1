78c75ea5dd52acbf41bb70f3a48cb0f0890fe0de
37955 <146433509+37955@users.noreply.github.com>
2024-02-27 10:47:37 +0100

    Time

---
+++ Extra/Time/Time/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(DateTime.Now.ToLongTimeString());
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
