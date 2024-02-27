25158e7c8197d3c78c43a519247d3e49a5d431fc
37955 <146433509+37955@users.noreply.github.com>
2024-02-27 10:37:54 +0100

    HelloWord

---
+++ Extra/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            string helloWorld = "Hello, World!";
+            for (int i = 0; i < helloWorld.Length; i++)
+            {
+                Console.Write(helloWorld[i]);
+                Thread.Sleep(350);
+            }
+
+        }
+    }
+}
